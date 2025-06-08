6ea919d53db47f073c89360be5930b4e527aa0e3
Ethan Baldwin <70599620+ethan-baldwin@users.noreply.github.com>
2025-06-08 10:54:22 -0400

    create script for running primary assembly

---
+++ assembly/hifiasm_primary.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=hifiasm
+#SBATCH --partition=highmem_p
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=400gb
+#SBATCH --time=150:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=eab77806@uga.edu
+#SBATCH --output=/scratch/eab77806/logs/hifiasm.%x_%j.out
+#SBATCH --error=/scratch/eab77806/logs/hifiasm.%x_%j.error
+
+OUTDIR="/scratch/eab77806/jim_projects/ipomoea"
+if [ ! -d $OUTDIR ]
+then
+    mkdir -p $OUTDIR
+fi
+cd $OUTDIR
+
+ml hifiasm/0.24.0-GCCcore-12.3.0
+
+hifiasm -o hifiasm -t $SLURM_CPUS_PER_TASK hifi/out.fastq.gz
