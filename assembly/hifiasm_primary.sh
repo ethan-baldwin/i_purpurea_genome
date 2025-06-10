c2a9d67dc8733606dde342391dfd96a5f7a236f6
Ethan Baldwin <70599620+ethan-baldwin@users.noreply.github.com>
2025-06-10 10:22:11 -0400

    add commands to calculate stats after assembly

--- assembly/hifiasm_primary.sh
+++ assembly/hifiasm_primary.sh
@@ -11,7 +11,7 @@
 #SBATCH --output=/scratch/eab77806/logs/hifiasm.%x_%j.out
 #SBATCH --error=/scratch/eab77806/logs/hifiasm.%x_%j.error
 
-OUTDIR="/scratch/eab77806/jim_projects/ipomoea"
+OUTDIR="/scratch/eab77806/jim_projects/ipomoea/assembly_primary"
 if [ ! -d $OUTDIR ]
 then
     mkdir -p $OUTDIR
@@ -21,3 +21,10 @@ cd $OUTDIR
 ml hifiasm/0.24.0-GCCcore-12.3.0
 
 hifiasm -o hifiasm -t $SLURM_CPUS_PER_TASK --primary hifi/out.fastq.gz
+awk '/^S/{print ">"$2;print $3}' hifiasm.p_ctg.gfa > hifiasm.p_ctg.fa
+
+ml GenomeTools
+gt seqstat hifiasm.p_ctg.fa > hifiasm.p_ctg.fa.stats
+
+ml SAMtools
+samtools faidx hifiasm.p_ctg.fa
