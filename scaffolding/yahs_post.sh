4b670b0717d59cdd8c30443649c1aac9c76e1d2a
Ethan Baldwin <70599620+ethan-baldwin@users.noreply.github.com>
2025-06-11 13:52:36 -0400

    run yahs post w/ no error correction

--- scaffolding/yahs_post.sh
+++ scaffolding/yahs_post.sh
@@ -21,4 +21,4 @@ ml YaHS/1.1-GCC-11.3.0
 
 assembly='/scratch/eab77806/jim_projects/ipomoea/assembly_primary/hifiasm.p_ctg.fa'
 
-juicer post -o yahs_final yahs.review.assembly yahs.liftover.agp $assembly
+juicer post -o yahs_final yahs_no_ec.review.assembly yahs_no_ec.liftover.agp $assembly
